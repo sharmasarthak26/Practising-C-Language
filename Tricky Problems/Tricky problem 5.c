b3fd540229ae5cecd2ca184e3d5e9a478d807870
Manikumar Tharmaraj <manikumartharmaraj@gmail.com>
2020-07-08 08:33:31 +0530

    Add files via upload

---
+++ Tricky Problems/Tricky problem 5.c
@@ -0,0 +1,73 @@
+Write a Program to get a string from the user, and a word  from the user.
+Remove the word from string and print the string. Write the program using functions.	
+	
+
+Ex:- Enter the string :- My name myself is Manikumar Tharmaraj
+
+     Enter the word:- myself
+
+     String after removed the word:- My name is Manikumar Tharmaraj
+
+C program:-
+
+#include<stdio.h>
+
+#include<string.h>
+
+char* removeword(char str[],char word[]);
+
+int main()
+
+{
+   
+	 char str[100],word[100];
+ 
+         printf("Enter the string\n");
+  
+         scanf("%[^\n]%*c",str);
+  
+         printf("Enter the word\n");
+    
+         scanf("%[^\n]%*c",word);
+     
+    removeword(str,word);
+ 
+         printf("string after removed the word:-\n%s",str);
+     
+    return 0;
+
+}
+
+char* removeword(char str[],char word[])
+
+{  
+   
+	 int i=0,j=0,k;
+ 
+         for(i=0;i<strlen(str);i++) 
+     
+    {
+        
+		while(str[i]==word[j] && word[j]!='\0') //to search the word in string
+
+	        {
+           
+			 for(k=i;str[k]!='\0';k++)
+ 
+                         {
+                
+				str[k]=str[k+1]; //to override the word by next words
+  
+	                 }
+        
+                         j++;
+        
+        }
+     
+    }
+
+         return str;
+
+}
+
+
---
+++ Tricky Problems/Tricky problem 7.c
@@ -0,0 +1,50 @@
+Write a program to Print the number of zeroes you encounter between the  numbers 0 to 1000
+
+Ex:-
+
+      Output:- 193
+
+C Program:-
+
+#include <stdio.h> 
+
+int main() 
+
+{ 
+  	 
+	int i,n,k,count=1;
+
+        for(i=0;i<=1000;i++)
+ 
+	  {
+       
+		n=i;
+
+         
+       while(n>0)
+ 
+		{
+      
+                 k = n % 10;
+ 
+                 if(k==0)
+  
+        	    {
+        
+		       //printf("k:%d\n",i);
+  
+                       count++;
+    
+                    }
+           
+                 n=n/10;
+
+                }
+
+          }
+ 
+          printf("count:%d",count);
+	  return 0;
+
+}
+
